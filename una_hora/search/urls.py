f39b050eb6df94a8d3b7d9e99120e63b16e874d6
leny133 <puertorican.dev@gmail.com>
2021-10-19 19:49:58 -0400

    serach app views function created
    Changed the urls name for this function and added the form to the template.

--- una_hora/search/urls.py
+++ una_hora/search/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.search_page, name="search_page"),
+    path("", views.search_result, name="search_result"),
 ]
--- una_hora/search/views.py
+++ una_hora/search/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
-
+from ..users.models import User
+from django.contrib.postgres.search import SearchVector
 
 # Create your views here.
-def search_page(request):
-    print("hola")
-    return render(request, "./search/search_page.html")
+def search_result(request):
+    query = request.GET['q']
+    user_result = User.objects.annotate(
+        search=SearchVector(
+            'email',
+            'full_name',
+            'tags',
+        ).filter(search=query)
+    )
+    if not user_result:
+        message = "Mentor "+ query +" not found."
+        return render(request, "./search/search_page.html",{
+                "message": message,
+            })
+    else:
+        message = "Mentors found: "
+        return render(request, "./search/search_page.html",{
+            "message": message,
+            "user_result": user_result,
+            })
